ef73d848f2e948ffa13d80a17b3913219d682037
Pavan Inguva <inguvakp@mit.edu>
2021-07-11 21:56:17 -0400

    ADD: Working model_2_exact finite difference and function eval

--- model_2_exact.m
+++ model_2_exact.m
@@ -7,35 +7,27 @@
 % On the left end of the domain, a modified Dirichlet B.C. is enforced
 % which sets the value of the Ghost node at 0 i.e. f(0) = 0
 
-%This scheme employs two variable transforms to solve the PBM exactly
+%This scheme employs two variable transforms to solve the PBM exactly and
+%uses a function evaluation for the transformed PBM. 
 
-function [f, mesh, varargout] = model_2_exact(N_cells, f0fun, ufun, zfun, xfun, x_vec, t_vec, varargin)
+function [f, mesh] = model_2_exact(N_cells, f0_transfun, ufun, zfun, xfun, x_vec, t_end)
 
 %% Description
 
 %INPUTS
 %N_cells: specify the number of cells for the mesh
-%f0fun: initial profile as a function handle
-%ufun: input u(x) as a function handle
+%f0_transfun: Input f0(x).*u(x) as a function handle 
+%ufun: Input u(x) as a function handle
 %zfun: z(x) is defined as int_{0}^{x} (1/(u(k)) dk. Input as a function
 %handle
 %xfun: compute the inverse of zfun i.e. x(z) offline and input as a function handle
 %x_vec: array containing the limits of the domain in x
-%t_Vec: array containing the start and stop time
-
-%varargin: This is to mainly deal with the different forms of outputs. Type
-%in "output_style" followed by one of the following options: 
-%"all": Outputs f containing the solution at each timestep
-%"final": only outputs the final timestep
-%"stride": outputs the solution at every nth time. Specify n as the next
-%argument
+%t_end: Specify the ending time of the simulation.
 
 %OUTPUTS
 %f is the array with the solution. Depending on the output style, there
 %would be different formats
 %mesh is the computed mesh. 
-%varargout would only contain the list of times outputted in the event
-%stride is used.
 
 %% Code
 
@@ -43,55 +35,26 @@ function [f, mesh, varargout] = model_2_exact(N_cells, f0fun, ufun, zfun, xfun, 
 z_mesh = linspace(zfun(x_vec(1)), zfun(x_vec(2)), N_cells);
 x_mesh = xfun(z_mesh);
 
-%Construct f0 
-f0_untransformed = f0fun(x_mesh);
+%compute functions
 u_vals = ufun(x_mesh);
-%Transformed f0 = f0(x).*u(x)
-f0_trans = f0_untransformed.*u_vals;
 
-%compute dz and dt
-dz = z_mesh(2) - z_mesh(1);
-dt = dz;
-
-%Initialize counter etc
-f_trans_old = f0_trans;
-t = t_vec(1);
-counter = 1;
-
-%perform timestepping using while loop
-
-while t < t_vec(2)
-    %Update first node: 
-    f_trans_new(1) = 0;
-    %update the rest
-    for i = 2:N_cells
-       f_trans_new(i) = f_trans_old(i-1); 
-    end
-    
-    %update counters etc
-    counter = counter +1
-    t = t + dt;
-    f_trans_old = f_trans_new;
-    
-    %outputs
-    if varargin{2} == "all"
-        f(counter,:) = f_trans_new./u_vals;
-    elseif varargin{2} == "stride"
-        if mod((counter -1), varargin{3}) == 0
-           f((((counter-1)/varargin{3}) +1),:) = f_trans_new./u_vals;
-           stride_vec((((counter-1)/varargin{3}) +1)) = t;
-        end
+%construct exact solution
+for i = 1:length(x_mesh)
+    if z_mesh(i) < z_mesh(1) + t_end
+        f_trans(i) = 0;
+    else
+        f_trans(i) = f0_transfun(xfun(z_mesh(i) -t_end));
     end
 end
 
-%output 
-if varargin{2} == "all" | varargin{2} == "stride"
-    f(1,:) = f0_untransformed;
-else
-    f = f_trans_new./u_vals;
-end
-stride_vec(1) = t_vec(1);
-varargout{1} = stride_vec;
+
+%Outputs
 mesh = x_mesh;
+f = f_trans./u_vals;
 
+        
 end
+
+
+
+
